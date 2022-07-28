fbed07f3cab4550b5bedcb0de3f2a2610f7a597a
Rimonteil <nardiotencha@gmail.com>
2022-07-28 16:24:05 +0200

    installed user + AUTH_USER_MODEL

--- EpicEvent/EpicEvent/settings.py
+++ EpicEvent/EpicEvent/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# CUSTOM ADDED
+AUTH_USER_MODEL = 'users.User'
