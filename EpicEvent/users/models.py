efd2b75a11b452ec99484b8d2cf0b26d662c6e1b
Rimonteil <nardiotencha@gmail.com>
2022-07-28 16:24:56 +0200

    user model + user manager

--- EpicEvent/users/models.py
+++ EpicEvent/users/models.py
@@ -1,3 +1,65 @@
-from django.db import models
+from django.db import models, transaction
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Un email est necessaire.')
+
+        elif not password:
+            raise ValueError('Un mot de passe est necessaire.')
+
+        try:
+            with transaction.atomic():
+                user = self.model(email=email, **extra_fields)
+                user.set_password(password)
+                user.save(using=self._db)
+                return user
+        except:
+            raise
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    class Role(models.TextChoices):
+        BLK = "", "",
+        SLR = "1", "SELLER"
+        SPT = "2", "SUPPORT"
+
+    email = models.EmailField(max_length=100, unique=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    phone = models.CharField(max_length=20)
+    role = models.CharField(
+        max_length=2,
+        choices=Role.choices,
+        default=Role.BLK
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        return self
