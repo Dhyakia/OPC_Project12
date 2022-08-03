f4a740614639ba37132e792ffbf347d113ae2e47
Rimonteil <nardiotencha@gmail.com>
2022-08-03 18:06:59 +0200

    Client, Contract & Event models added

--- EpicEvent/events/admin.py
+++ EpicEvent/events/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models import Client, Contract, Event
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('contact_info', 'is_client', 'sales_contact')
+
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ('contract_status', 'amount', 'client', 'sales_contact')
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_status', 'notes', 'client', 'support_contact')
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Contract, ContractAdmin)
+admin.site.register(Event, EventAdmin)
--- EpicEvent/events/models.py
+++ EpicEvent/events/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Client(models.Model):
+
+    contact_info = models.CharField(max_length=200)
+    is_client = models.BooleanField(default=False)
+
+    sales_contact = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    
+
+class Contract(models.Model):
+
+    amount = models.IntegerField()
+    contract_status = models.BooleanField(default=False)
+
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    sales_contact = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+
+
+class Event(models.Model):
+
+    notes = models.CharField(max_length=2000)
+    event_status = models.BooleanField(default=False)
+
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    support_contact = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
