0a900c4361937ec248fac1bcb4df3a5256a872f8
Rimonteil <nardiotencha@gmail.com>
2022-08-05 18:04:45 +0200

    added viewsets for client/contract/event

--- EpicEvent/events/views.py
+++ EpicEvent/events/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from events.models import Client, Contract, Event
+from events.serializers import ClientSerializer, ContractSerializer, EventSerializer
+
+
+class ClientViewset(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = ClientSerializer
+    dataset = Client.objects.all()
+
+    # TODO: write the calls
+
+
+class ContractViewset(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = ContractSerializer
+    dataset = Contract.objects.all()
+
+    # TODO: write the calls
+
+
+class EventViewset(ModelViewSet):
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = EventSerializer
+    dataset = Event.objects.all()
+
+    # TODO: write the calls
