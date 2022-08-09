d3bd9371a5ecfb81411955c88ec4fd053ae0539c
Rimonteil <nardiotencha@gmail.com>
2022-08-09 16:54:46 +0200

    setup the urls & routers for the API

--- EpicEvent/EpicEvent/urls.py
+++ EpicEvent/EpicEvent/urls.py
@@ -1,21 +1,20 @@
-"""EpicEvent URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from multiprocessing.connection import Client
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
+from users.views import UsersViewset
+from events.views import ClientsViewset, ContractsViewset, EventsViewset
+
+
+router = routers.SimpleRouter()
+router.register(r'users', UsersViewset)
+router.register(r'clients' ,ClientsViewset)
+router.register(r'contracts', ContractsViewset)
+router.register(r'events', EventsViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
