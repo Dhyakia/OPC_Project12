903ffae1b92e976594f7f970ce7341d7c434fa14
Rimonteil <nardiotencha@gmail.com>
2022-08-09 16:55:59 +0200

    list/create/retrieve/update/destroy for 3 viewsets

--- EpicEvent/events/views.py
+++ EpicEvent/events/views.py
@@ -1,33 +1,173 @@
-from django.shortcuts import render
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from events.models import Client, Contract, Event
 from events.serializers import ClientSerializer, ContractSerializer, EventSerializer
 
 
-class ClientViewset(ModelViewSet):
+class ClientsViewset(ModelViewSet):
 
     permission_classes = [IsAuthenticated]
     serializer_class = ClientSerializer
-    dataset = Client.objects.all()
+    queryset = Client.objects.all()
+    
+    def list(self, request):
+        query_client = Client.objects.all()
+        serializer_client = ClientSerializer(query_client, many=True)
+        return Response(serializer_client.data, status.HTTP_200_OK)
 
-    # TODO: write the calls
+    def create(self, request):
+        client = request.data
+        serializer_client = ClientSerializer(data=client)
+
+        if serializer_client.is_valid(raise_exception=True):
+            serializer_client.save()
+            return Response(serializer_client.data, status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        query_client = Client.objects.get(id=pk)
+        serializer_client = ClientSerializer(query_client)
+        return Response(serializer_client.data, status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        if Client.objects.filter(id=pk).exists():
+            query_client = Client.objects.filter(id=pk)
+            new_contact_info = request.data['contact_info']
+            new_is_client = request.data['is_client']
+            new_sales_contact = request.data['sales_contact']
+
+            query_client.update(
+                contact_info=new_contact_info,
+                is_client=new_is_client,
+                sales_contact=new_sales_contact
+            )
+            serializer_client = ClientSerializer(query_client, many=True)
+            return Response(serializer_client.data, status.HTTP_202_ACCEPTED)
+
+        else:
+            message = "Pas ou plus d'utilisateur à cette adresse"
+            return Response(message, status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        if Client.objects.filter(id=pk).exists():
+            query_client = Client.objects.filter(id=pk)
+            query_client.delete()
+            message = "Client supprimé"
+            return Response(message, status.HTTP_204_NO_CONTENT)
+
+        else:
+            message = "Pas ou plus de client à cette adresse"
+            return Response(message, status.HTTP_404_NOT_FOUND)
 
 
-class ContractViewset(ModelViewSet):
+class ContractsViewset(ModelViewSet):
 
     permission_classes = [IsAuthenticated]
     serializer_class = ContractSerializer
-    dataset = Contract.objects.all()
+    queryset = Contract.objects.all()
 
-    # TODO: write the calls
+    def list(self, request):
+        query_contract = Contract.objects.all()
+        seriallizer_contract = ContractSerializer(query_contract, many=True)
+        return Response(seriallizer_contract.data, status.HTTP_200_OK)
+
+    def create(self, request):
+        contract = request.data
+        serializer_contract = ContractSerializer(data=contract)
+
+        if serializer_contract.is_valid(raise_exception=True):
+            serializer_contract.save()
+            return Response(serializer_contract.data, status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        query_contract = Contract.objects.get(id=pk)
+        serializer_contract = ContractSerializer(query_contract)
+        return Response(serializer_contract.data, status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        if Contract.objects.filter(id=pk).exists():
+            query_contract = Contract.objects.filter(id=pk)
+            new_amount = request.data["amount"]
+            new_contract_status = request.data["contract_status"]
+            new_sales_contact = request.data["sales_contact"]
+
+            query_contract.update(
+                amount=new_amount,
+                contract_status=new_contract_status,
+                sales_contact=new_sales_contact
+            )
+            serializer_contract = ContractSerializer(query_contract, many=True)
+            return Response(serializer_contract.data, status.HTTP_202_ACCEPTED)
+
+        else:
+            message = "Pas ou plus d'utilisateur à cette adresse"
+            return Response(message, status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        if Contract.objects.filter(id=pk).exists():
+            query_contract = Contract.objects.filter(id=pk)
+            query_contract.delete()
+            message = "Contrat supprimé"
+            return Response(message, status.HTTP_204_NO_CONTENT)
+
+        else:
+            message = "Pas ou plus de contrat à cette adresse"
+            return Response(message, status.HTTP_404_NOT_FOUND)
 
 
-class EventViewset(ModelViewSet):
+class EventsViewset(ModelViewSet):
 
     permission_classes = [IsAuthenticated]
     serializer_class = EventSerializer
-    dataset = Event.objects.all()
+    queryset = Event.objects.all()
 
-    # TODO: write the calls
+    def list(self, request):
+        query_event = Event.objects.all()
+        serializer_event = EventSerializer(query_event, many=True)
+        return Response(serializer_event.data, status.HTTP_200_OK)
+
+    def create(self, request):
+        event = request.data
+        serializer_event = EventSerializer(data=event)
+
+        if serializer_event.is_valid(raise_exception=True):
+            serializer_event.save()
+            return Response(serializer_event.data, status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        query_event = Event.objects.get(id=pk)
+        serializer_event = EventSerializer(query_event)
+        return Response(serializer_event.data, status.HTTP_200_OK)
+
+    def update(self, request, pk=None):
+        if Event.objects.filter(id=pk).exists():
+            query_event = Event.objects.filter(id=pk)
+            new_notes = request.data['notes']
+            new_event_status = request.data['event_status']
+            new_support_contact = request.data['support_contact']
+
+
+            query_event.update(
+                notes=new_notes,
+                event_status=new_event_status,
+                support_contact=new_support_contact
+            )
+            serializer_event = EventSerializer(query_event, many=True)
+            return Response(serializer_event.data, status.HTTP_202_ACCEPTED)
+        
+        else:
+            message = "Pas ou plus d'utilisateur à cette adresse"
+            return Response(message, status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, pk=None):
+        if Event.objects.filter(id=pk).exists():
+            query_event = Event.objects.filter(id=pk)
+            query_event.delete()
+            message = "Evenement supprimé"
+            return Response(message, status.HTTP_204_NO_CONTENT)         
+    
+        else:
+            message = "Pas ou plus d'évenement à cette adresse"
+            return Response(message, status.HTTP_404_NOT_FOUND)
