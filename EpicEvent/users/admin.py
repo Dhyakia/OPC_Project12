76737053b4d51697cee4a729e6c10d96027c2a1d
Rimonteil <nardiotencha@gmail.com>
2022-08-12 16:33:38 +0200

    Renamed of class UserAdmin to MyUserAdmin

--- EpicEvent/users/admin.py
+++ EpicEvent/users/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
 
 
-class UserAdmin(admin.ModelAdmin):
+class MyUserAdmin(admin.ModelAdmin):
     list_display = ['id', 'email', 'password', 'role', 'date_created', 'date_updated', 'is_superuser']
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, MyUserAdmin)
