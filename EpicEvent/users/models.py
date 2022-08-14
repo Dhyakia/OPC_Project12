fcbad70b49964f30a037e91551b87877d5ab4b5d
Rimonteil <nardiotencha@gmail.com>
2022-08-14 16:50:40 +0200

    spacing

--- EpicEvent/users/models.py
+++ EpicEvent/users/models.py
@@ -45,7 +45,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=100, unique=True)
     role = models.CharField(max_length=1, choices=Role.choices,)
 
-    # optional
     # auto_add
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
